2e7ecee96190322a8c890a663dbefc1fb8a2c905
Martin Ades <martin.ades13@gmail.com>
2020-03-03 12:21:37 -0300

    Primera subida de archivos

---
+++ assets/scripts/app.js
@@ -0,0 +1,48 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+let logOperatorUsed = [];
+
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`; //output text generated
+    outputResult(currentResult, calcDescription);
+}
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+    logEntries.push(enteredNumber);
+}
+
+function substract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', substract);
+divideBtn.addEventListener('click', divide);
+multiplyBtn.addEventListener('click', multiply);

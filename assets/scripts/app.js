92455fda6cf5e237e29cbed34529e742acac10ff
Martin Ades <martin.ades13@gmail.com>
2020-03-03 16:08:35 -0300

    Función para logear operaciones

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,19 +12,23 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     outputResult(currentResult, calcDescription);
 }
 
+function writeToLog(operationIdentifier, prevResult, operationNumber, newResult) {
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
 function add() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult += enteredNumber;
     createAndWriteOutput('+', initialResult, enteredNumber);
-    const logEntry = {
-        operation: 'add',
-        prevResult: initialResult,
-        number: enteredNumber,
-        result: currentResult 
-    };
-    logEntries.push(logEntry);
-    console.log(logEntries);
+    writeToLog('add', initialResult, enteredNumber, currentResult);
 }
 
 function substract() {
@@ -32,6 +36,7 @@ function substract() {
     const initialResult = currentResult;
     currentResult -= enteredNumber;
     createAndWriteOutput('-', initialResult, enteredNumber);
+    writeToLog('substract', initialResult, enteredNumber, currentResult);
 }
 
 function multiply() {
@@ -39,6 +44,7 @@ function multiply() {
     const initialResult = currentResult;
     currentResult *= enteredNumber;
     createAndWriteOutput('*', initialResult, enteredNumber);
+    writeToLog('multiply', initialResult, enteredNumber, currentResult);
 
 }
 
@@ -47,6 +53,7 @@ function divide() {
     const initialResult = currentResult;
     currentResult /= enteredNumber;
     createAndWriteOutput('/', initialResult, enteredNumber);
+    writeToLog('divide', initialResult, enteredNumber, currentResult);
 }
 
 addBtn.addEventListener('click', add);

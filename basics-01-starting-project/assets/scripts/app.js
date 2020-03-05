3bba3e044250eb0f524d3122c42e519f541cf2bb
Martin Ades <martin.ades13@gmail.com>
2020-03-05 11:23:57 -0300

    Created folders for differents and new projects

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,73 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+let logOperatorUsed = [];
+
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`; //output text generated
+    outputResult(currentResult, calcDescription);
+}
+
+function writeToLog(operationIdentifier, prevResult, operationNumber, newResult) {
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
+function calculateResult(calculationType) {
+    const enteredNumber = getUserNumberInput();
+    if (calculationType != 'add' && calculationType != 'substract' && calculationType != 'multiply' 
+    && calculationType != 'divide' || !enteredNumber) {
+        return;
+    }
+    const initialResult = currentResult;
+    let mathOperator;
+    if (calculationType === 'add')  {
+        currentResult += enteredNumber;
+        mathOperator = '+';
+    } else if (calculationType === 'substract'){
+        currentResult -= enteredNumber;
+        mathOperator = '-';
+    } else if (calculationType === 'multiply'){
+        currentResult += enteredNumber;
+        mathOperator = '*';
+    } else if (calculationType === 'divide'){
+        currentResult /= enteredNumber;
+        mathOperator = '/';
+    }
+
+
+    createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+    writeToLog('add', initialResult, enteredNumber, currentResult);
+
+}
+
+function add() {
+   calculateResult('add')
+}
+
+function substract() {
+    calculateResult('substract')
+}
+
+function multiply() {
+    calculateResult('multiply')
+}
+
+function divide() {
+    calculateResult('divide')
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', substract);
+divideBtn.addEventListener('click', divide);
+multiplyBtn.addEventListener('click', multiply);
